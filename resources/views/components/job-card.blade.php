f1940c2b842bd5719f5cce3822ec7a12cc3edc55
Palash Kumer <palashkumer237@gmail.com>
2025-04-22 17:19:49 +0600

    initial commit with frontend markup

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sn">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer </h3>
+        <p>Full Time - From $50,000 </p>
+    </div>
+    <div class="flex justify-between items-center my-auto">
+        <div>
+            <x-tag> Tag </x-tag>
+            <x-tag> Tag </x-tag>
+            <x-tag> Tag </x-tag>
+        </div>
+        <img src="{{ Vite::asset('resources/images/logo_updated.svg') }}" alt="">
+    </div>
+
+</div>
