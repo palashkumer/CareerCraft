68ba85539d130e2db150dee629840442d51261de
Palash Kumer <palashkumer237@gmail.com>
2025-04-25 03:13:10 +0600

    add job search box

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SearchController;
 
 Route::get('/', [JobController::class, 'index']);
+// Route::get('/search', [SearchController::class]);
+Route::get('/search', SearchController::class); 
+
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
